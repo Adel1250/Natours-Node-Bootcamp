3baa93658f99afa74b75e03d2d22306828fefbb9
Adel1250 <adelashrafhamouda@gmail.com>
2022-10-13 22:14:18 +0200

    Mongoose Intro

--- server.js
+++ server.js
@@ -1,7 +1,39 @@
+const mongoose = require('mongoose');
 const dotenv = require("dotenv");
 dotenv.config({ path: './config.env' });
-
 const app = require(`${__dirname}/app`);
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose.connect(DB, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(() => console.log("DB Connected Successfully"));
+
+const tourSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "A tour must have a name"],
+        unique: true
+    },
+    price: {
+        type: Number,
+        required: [true, "A tour must have a price"]
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+    name: "The Forest Hiker",
+    rating: 4.7,
+    price: 497
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
