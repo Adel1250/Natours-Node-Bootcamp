cecfe998691177e08d85717d917443ce2fce1305
Adel1250 <adelashrafhamouda@gmail.com>
2022-10-26 20:52:08 +0200

    Changing Model

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,14 +6,50 @@ const tourSchema = mongoose.Schema({
         required: [true, "A tour must have a name"],
         unique: true
     },
+    duration: {
+        type: Number,
+        required: [true, "A tour must have a duration"]
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, "A tour ,ust have a maximum group size"]
+    },
     price: {
         type: Number,
         required: [true, "A tour must have a price"]
     },
-    rating: {
+    difficulty: {
+        type: String,
+        required: [true, "A tour must have a diffiuclty"]
+    },
+    ratingsAverage: {
         type: Number,
         default: 4.5
-    }
+    },
+    ratingsQuantity: {
+        type: Number,
+        default: 0
+    },
+    priceDiscount: Number,
+    summary: {
+        type: String,
+        trim: true,
+        required: [true, "A tour must have a summary"]
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    imageCover: {
+        type: String,
+        required: [true, "A tour must have a cover image"]
+    },
+    images: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    startDates: [Date]
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
